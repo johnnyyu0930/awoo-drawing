38325e12256abcc8131b537728546f20c71a1d02
johnnyyu0930 <johnnyyu@awoo.com.tw>
2023-01-04 23:10:35 +0800

    add deploy yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/awoo-drawing/',
   plugins: [vue(), WindiCSS()],
   server: {
     port: 8888,
